9aa523a7e1b272e0f21248f8c7405f3b7c8421b9
Angel5764 <116742740+Angel5764@users.noreply.github.com>
2023-05-24 16:29:42 -0500

    Agregar archivos de proyecto.

---
+++ Funciones.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fact_Recursivo
+{
+    public class Funciones
+    {
+        public int Fact(int n)
+        {
+            if(n==0) return 1;
+            else return Fact(n-1)*n;
+        }
+        public int FactCiclo(int n)
+        {
+            int factorial = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                factorial=factorial*i;
+            }
+            return factorial;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Fact_Recursivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funciones factorial = new Funciones();
+            Console.WriteLine("Recursivo: ");
+            Console.WriteLine(factorial.Fact(7));
+            Console.WriteLine(factorial.Fact(9));
+            Console.WriteLine(factorial.Fact(12));
+            Console.WriteLine("Con ciclos: ");
+            Console.WriteLine(factorial.FactCiclo(7));
+            Console.WriteLine(factorial.FactCiclo(9));
+            Console.WriteLine(factorial.FactCiclo(12));
+        }
+    }
+}
